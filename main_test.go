8c33eb3954fc3f7fdcfca1bd96e7e57a6b04f256
Murad <guseinovmg@gmail.com>
2024-06-14 14:46:14 +0300

    add sqlite driver

---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"testing"
+	"time"
+)
+
+func Test_main(t *testing.T) {
+	fmt.Println(math.MaxInt64)
+	//18446744073709551615
+	//9223372036854775807
+	//1718200199829214
+	fmt.Println(time.UnixMicro(math.MaxInt64))
+	fmt.Println(time.Now().UnixMicro())
+
+	userIdBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(userIdBytes, uint64(time.Now().UnixMicro()))
+	log.Println(userIdBytes)
+}
